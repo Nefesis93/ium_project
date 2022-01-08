0f6d3d769f2d0cfe7d196e117d4688aa4f32f56b
nefesis93 <lucamattei93@gmail.com>
2022-01-08 19:30:18 +0100

    aggiunti poup sul bottone di password dimenticata

--- lib/login.dart
+++ lib/login.dart
@@ -3,6 +3,122 @@ import 'package:ium_project/login_info.dart';
 import 'package:ium_project/custom_animations.dart';
 
 /*
+funzione che costruisce il dialog che permette di inserire la mail nel caso di password dimenticata
+*/
+void _forgotPasswordDialog(BuildContext context) {
+  showDialog<String>(
+    context: context,
+    builder: (BuildContext context) => AlertDialog(
+      title: const Text(
+        "Recupero password",
+        textAlign: TextAlign.center,
+      ),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: const  <Widget>[
+          Text(
+            "Inserire la tua email per il recupero della password",
+            textAlign: TextAlign.center,
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
+            child:TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Email",
+                hintText: "Inserisci la tua email"
+              )
+            )
+          )
+        ],
+      ),
+      actionsAlignment: MainAxisAlignment.center,
+      actions: <Widget>[
+        Container(
+          height: 40,
+          width: 150,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(10)
+          ),
+          child: TextButton(
+            onPressed: () {
+              //da inserire condizione se testo è vuoto oppure no if
+              Navigator.pop(context);
+              _emailDialog(context);
+              //else
+              //_missingEmailDialog(context);
+            },
+            child: const Text(
+              "Invia",
+              style: TextStyle(
+                color: Colors.white
+              )
+            )
+          )
+        ),
+      ],
+    )
+  );
+}
+
+/*
+funzione che costruisce il dialog che conferma l'inserimento dell'indirizzo email
+*/
+void _emailDialog(BuildContext context) {
+  showDialog<String>(
+    context: context,
+    builder: (BuildContext context) => AlertDialog(
+      title: const Text(
+        "Email inviata",
+        textAlign: TextAlign.center,
+      ),
+      content: const Text(
+        "Abbiamo inviato una email al tuo indirizzo di posta elettronica per recuperare la password",
+        textAlign: TextAlign.center,
+      ),
+      actionsAlignment: MainAxisAlignment.end,
+      actions: <Widget>[
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text("ok"),
+        ),
+      ],
+    )
+  );
+}
+
+/*
+funzione che costruisce il dialog che conferma l'inserimento dell'indirizzo email
+*/
+void _missingEmailDialog(BuildContext context) {
+  showDialog<String>(
+    context: context,
+    builder: (BuildContext context) => AlertDialog(
+      title: const Text(
+        "Inserire email",
+        textAlign: TextAlign.center,
+      ),
+      content: const Text(
+        "Devi prima inserire l'email per recuperare la password",
+        textAlign: TextAlign.center,
+      ),
+      actionsAlignment: MainAxisAlignment.end,
+      actions: <Widget>[
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text("ok"),
+        ),
+      ],
+    )
+  );
+}
+
+/*
 schermata di login
 */
 class LoginPage extends StatelessWidget {
@@ -40,7 +156,9 @@ class LoginPage extends StatelessWidget {
               )
             ),
             TextButton(
-              onPressed: () => 0,
+              onPressed: () {
+                _forgotPasswordDialog(context);
+              },
               child: const Text(
                 "Forgot Password",
                 style: TextStyle(color: Colors.blue, fontSize: 15),
@@ -67,6 +185,16 @@ class LoginPage extends StatelessWidget {
                     color: Colors.white,
                     fontSize: 25
                   )
+                )
+              )
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextButton(
+                onPressed: () => 0,
+                child: const Text(
+                  "Non hai un account? Iscriviti",
+                  style: TextStyle(color: Colors.blue, fontSize: 12),
                 )
               )
             )
