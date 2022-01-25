4c5698bf47d606a111f77247ea2257ea934497a1
ValerioLP <valeriomezzoprete@gmail.com>
2022-01-25 14:02:26 +0100

    passo la palla a Luca

--- lib/pages/pagina_appunto/lock.dart
+++ lib/pages/pagina_appunto/lock.dart
@@ -4,29 +4,33 @@ import 'package:ium_project/utility/bars/bars.dart';
 class Lock extends StatelessWidget {
   const Lock({Key? key}) : super(key: key);
 
-  Widget createBody(BuildContext context) {
-    return ListView(shrinkWrap: true, children: <Widget>[
-      Container(
-          height: 90,
-          alignment: Alignment.center,
-          child: const Padding(
-            padding: EdgeInsets.all(0),
-            child: Text(
-              "Lock",
-              style: TextStyle(
-                color: Colors.blue,
-                fontSize: 40,
-              ),
-            ),
-          )),
-      Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-                padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
-                child: Column(
-                  //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        bottomNavigationBar: const DefaultBottomBar(),
+        floatingActionButton: const FloatingPlusButton(),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        body: ListView(shrinkWrap: true, children: <Widget>[
+          Container(
+              height: 90,
+              alignment: Alignment.center,
+              child: const Padding(
+                padding: EdgeInsets.all(0),
+                child: Text(
+                  "Lock",
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 40,
+                  ),
+                ),
+              )),
+          //riga contenente due colonne
+          Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Column(
                   children: const <Widget>[
                     Text(
                       "Valerio Mezzoprete",
@@ -36,26 +40,15 @@ class Lock extends StatelessWidget {
                       ),
                     ),
                     Text(
-                      "Prof Tolomei",
+                      "Prof. Tolomei",
                       style: TextStyle(
                         color: Colors.blue,
                         fontSize: 16,
                       ),
                     ),
                   ],
-                ))
-          ])
-    ]);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      bottomNavigationBar: const DefaultBottomBar(),
-      floatingActionButton: const FloatingPlusButton(),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      body: createBody(context),
-    );
+                )
+              ])
+        ]));
   }
 }
