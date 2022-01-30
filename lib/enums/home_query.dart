94f834b74365614d2d3471d88e8cac909cab3016
ValerioLP <valeriomezzoprete@gmail.com>
2022-01-30 14:43:17 +0100

    Scritto pseudo-codice su come verificare i vari casi delle query di ricerca. Luca controlla grazie e vedi come si può implementare

--- lib/enums/home_query.dart
+++ lib/enums/home_query.dart
@@ -10,6 +10,43 @@ class HomeQuery {
   }
 
   static const HomeQuery home = HomeQuery._([Topic.lock, Topic.algebra]);
-  static const HomeQuery prova = HomeQuery._([Topic.pdsi, Topic.prototyping]);
-  //qui va inserita una const con il risultato di ogni tipo di ricerca prevista dall'app
+
+  /*questo è il risultato della query solo facoltà con l'aggiunta di
+  prototyping se l'utente ha caricato l'appunto precedentemente*/
+  static const HomeQuery proto = HomeQuery._([
+    Topic.algebra,
+    Topic.algoritmi,
+    Topic.basi,
+    Topic.lock,
+    Topic.lockduefasi,
+    Topic.pdsi,
+    Topic.prototyping,
+    Topic.tipidilock,
+    Topic.turing
+  ]);
+
+  //questo è il risultato della query solo facoltà senza prototyping
+  static const HomeQuery facolta = HomeQuery._([
+    Topic.algebra,
+    Topic.algoritmi,
+    Topic.basi,
+    Topic.lock,
+    Topic.lockduefasi,
+    Topic.pdsi,
+    Topic.tipidilock,
+    Topic.turing
+  ]);
+
+  /*
+  Questo è il risultato delle query:
+  1) titolo-facoltà-prof
+  2) titolo-prof
+  3) titolo-facoltà
+  4) titolo
+  */
+  static const HomeQuery locks =
+      HomeQuery._([Topic.lock, Topic.lockduefasi, Topic.tipidilock]);
+
+  //questo è il risultato di tutte le altre query
+  static const HomeQuery lock = HomeQuery._([Topic.lock]);
 }
--- lib/pages/home/search.dart
+++ lib/pages/home/search.dart
@@ -7,56 +7,139 @@ import 'package:ium_project/utility/custom_animations.dart';
 class Search {
   static void searchDialog(BuildContext context) {
     showDialog<String>(
-      context: context,
-      builder: (BuildContext context) => AlertDialog(
-        //colonna che conterrà tutti i parametri di ricerca
-        actionsAlignment: MainAxisAlignment.end,
-        elevation: 0,
-        alignment: Alignment.topRight,
-        insetPadding: const EdgeInsets.all(0),
-        contentPadding: const EdgeInsets.all(0),
-        backgroundColor: Colors.transparent,
-        content: SizedBox(
-          width: 330,
-          height: 300,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              //riga Titolo`
-              _getInputRow(context, "Titolo"),
-              _getInputRow(context, "Facoltà"),
-              _getInputRow(context, "Corso"),
-              _getInputRow(context, "Prof"),
-              _getInputRow(context, "Autore"),
-              Container(
-                width: 150,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: const BorderRadius.horizontal(left: Radius.circular(30)),
-                  border: Border.all(
-                    color: Colors.black
-                  ),
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+              //colonna che conterrà tutti i parametri di ricerca
+              actionsAlignment: MainAxisAlignment.end,
+              elevation: 0,
+              alignment: Alignment.topRight,
+              insetPadding: const EdgeInsets.all(0),
+              contentPadding: const EdgeInsets.all(0),
+              backgroundColor: Colors.transparent,
+              content: SizedBox(
+                width: 330,
+                height: 300,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    //riga Titolo`
+                    _getInputRow(context, "Titolo"),
+                    _getInputRow(context, "Facoltà"),
+                    _getInputRow(context, "Corso"),
+                    _getInputRow(context, "Prof"),
+                    _getInputRow(context, "Autore"),
+                    Container(
+                      width: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: const BorderRadius.horizontal(
+                            left: Radius.circular(30)),
+                        border: Border.all(color: Colors.black),
+                      ),
+                      child: TextButton(
+                        onPressed: () {
+                          /*pseudo-codice
+
+                          //queste credo siano variabili da mettere fuori
+                          setMaterie = insieme di tutte le materie
+                          boolean queryTitolo = false;
+                          boolean queryFacolta = false;
+                          boolean queryProf = false;
+                          
+                          /*questa dovrebbe essere una variabile globale che si setta a true
+                          quando l'utente carica un appunto (prototyping)*/
+                          boolean queryProto = false;
+
+                          //questa variabile serve a capire se l'utente ha inserito un input
+                          riscontrabile nel database oppure no
+                          queryValide = false;
+                          
+                          if (titolo.getInput() != null) {
+                            for(materia in setMaterie)
+                              if titolo.getInput.lowerCase().equals(materia.getTitle())
+                                queryTitolo = true;
+                                queryValida = true;
+                          }
+
+                          if (facolta.getInput() != null) {
+                            for(materia in setMaterie)
+                              if facolta.getInput.lowerCase().equals(materia.getDepartment())
+                                queryFacolta = true;
+                                queryValida = true;
+                          }
+
+                          if (corso.getInput() != null) {
+                            for(materia in setMaterie)
+                              if corso.getInput.lowerCase().equals(materia.getTopic())                                
+                                queryValida = true;
+                          }
+
+                          if (prof.getInput() != null) {
+                            for(materia in setMaterie)
+                              if prof.getInput.lowerCase().equals(materia.getTeacher())
+                                queryProf = true;
+                                queryValida = true;
+                          }
+
+                          if (autore.getInput() != null) {
+                            for(materia in setMaterie)
+                              if titolo.getInput.lowerCase().equals(materia.getTitle())
+                                queryValida = true;
+                          }
+
+                          //caso in cui l'utente cerca le quattro query che danno le tre locks
+                          if ((queryTitolo) || 
+                              (queryTitolo && queryFacolta) || 
+                              (queryTitolo && queryProf) ||
+                              (queryTitolo && queryFacolta && queryProf))
+                            if (corso.getInput == null && autore.getInput == null)
+                              QueryState().setState(HomeQuery.locks);
+                            else { QueryState().setState(HomeQuery.lock); }
+                          
+                          //caso in cui l'utente cerca solo per facoltà prima di aver caricato un appunto
+                          elseif(queryFacolta && !queryProto)
+                            if(corso.getInput == null && autore.getInput == null
+                                    && titolo.getInput == null && prof.getInput == null)
+                              QueryState().setState(HomeQuery.facolta);
+                            else { QueryState().setState(HomeQuery.lock); }
+                          
+                          //caso in cui l'utente cerca solo per facoltà successivamente aver caricato un appunto
+                          elseif(queryFacolta && queryProto)
+                            if(corso.getInput == null && autore.getInput == null
+                                    && titolo.getInput == null && prof.getInput == null)
+                              QueryState().setState(HomeQuery.proto);
+                            else { QueryState().setState(HomeQuery.lock); }
+
+                          //tutti gli altri casi
+                          else {
+                            if(queryValida)
+                              QueryState().setState(HomeQuery.lock);
+                            else {
+                              pagina con scritto "Nessun risultato ottenuto"                            
+                            }
+                          }
+                          */
+
+                          /*quì prima c'era .prova e funzionava (2 elementi), se
+                          metti .lock che ha un solo elemento da index error*/
+                          QueryState().setState(HomeQuery.home);
+                          //Navigator.pop(context);
+                          Navigator.pushReplacement(context,
+                              CustomAnimations.flatAnimation(MyPage.home));
+                        },
+                        child: const Text(
+                          'Cerca',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-                child: TextButton(
-                  onPressed: () {
-                    QueryState().setState(HomeQuery.prova);
-                    //Navigator.pop(context);
-                    Navigator.pushReplacement(context, CustomAnimations.flatAnimation(MyPage.home));
-                  },
-                  child: const Text(
-                    'Cerca',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      )
-    );
+              ),
+            ));
   }
 
   /*
@@ -67,9 +150,7 @@ class Search {
       height: 50,
       decoration: BoxDecoration(
         borderRadius: const BorderRadius.horizontal(left: Radius.circular(30)),
-        border: Border.all(
-          color: Colors.black
-        ),
+        border: Border.all(color: Colors.black),
         color: Colors.blue,
       ),
       child: Row(
@@ -90,12 +171,11 @@ class Search {
             padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
             child: const TextField(
               decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                )
-              ),
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  )),
             ),
           )
         ],
